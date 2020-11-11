c689c1bf760a40c82cbfebb6bf02aacf7d0fbf96
James Brooks <james@alt-three.com>
2020-11-11 16:23:24 +0000

    wip

--- backup.sh
+++ backup.sh
@@ -3,66 +3,63 @@
 set -e
 
 BACKUP_STATUS=0
+BACKUP_TIMESTAMP=$(date +%Y%m%d%H%M%S)
+BACKUP_ARCHIVES=()
 
-# Change To Tmp Directory
+echo "Streaming backups to storage..."
 
-cd /tmp
+for DATABASE in $BACKUP_DATABASES; do
+    BACKUP_ARCHIVE_NAME="$DATABASE.sql.gz"
+    BACKUP_ARCHIVE_PATH="$BACKUP_FULL_STORAGE_PATH$BACKUP_TIMESTAMP/$BACKUP_ARCHIVE_NAME"
 
-# Make A Temporary Backup Directory
-
-mkdir -p /tmp/forge-backups/$BACKUP_ID
-
-# Run The Correct Backup Script For Each Database Driver
-
-if [[ $SERVER_DATABASE_DRIVER == 'mysql' ]]
-then
-
-    for DATABASE in $BACKUP_DATABASES; do
+    if [[ $SERVER_DATABASE_DRIVER == 'mysql' ]]
+    then
         mysqldump \
             --user=root \
             --password=$SERVER_DATABASE_PASSWORD \
             --single-transaction \
-            $DATABASE > /tmp/forge-backups/$BACKUP_ID/$DATABASE.sql
-    done
-
-elif [[ $SERVER_DATABASE_DRIVER == 'pgsql' ]]
-then
-    for DATABASE in $BACKUP_DATABASES; do
-        sudo -u postgres pg_dump --clean -F p $DATABASE > /tmp/forge-backups/$BACKUP_ID/$DATABASE.sql
-    done
-fi
-
-# Add SQL Dump To Archive And Remove It Afterwards
-
-tar -czvf $BACKUP_ARCHIVE --remove-files -C /tmp/forge-backups/$BACKUP_ID/ .
-
-# Remove The Temp Directory
-
-rm -rf /tmp/forge-backups/$BACKUP_ID
-
-# Upload The Archived File
-
-if [ -f $BACKUP_ARCHIVE ]
-then
-    echo "Uploading backup archive..."
-
-    aws s3 cp /tmp/$BACKUP_ARCHIVE $BACKUP_FULL_STORAGE_PATH \
-        --profile=$BACKUP_AWS_PROFILE_NAME \
-        ${BACKUP_AWS_ENDPOINT:+ --endpoint=$BACKUP_AWS_ENDPOINT}
-
-    # Set A Failed Status
-
-    if [ $? -ne 0 ]
+            -B \
+            $DATABASE | \
+            gzip -c | \
+            aws s3 cp - $BACKUP_ARCHIVE_PATH \
+            --profile=$BACKUP_AWS_PROFILE_NAME \
+            ${BACKUP_AWS_ENDPOINT:+ --endpoint=$BACKUP_AWS_ENDPOINT}
+    elif [[ $SERVER_DATABASE_DRIVER == 'pgsql' ]]
     then
-        echo "There was an error uploading the backup archive..."
-        BACKUP_STATUS=1
+        cd /tmp
+
+        sudo -u postgres pg_dump --clean --create -F p $DATABASE | \
+        gzip -c | \
+        aws s3 cp - $BACKUP_ARCHIVE_PATH \
+            --profile=$BACKUP_AWS_PROFILE_NAME \
+            ${BACKUP_AWS_ENDPOINT:+ --endpoint=$BACKUP_AWS_ENDPOINT}
     fi
 
-    # Remove Backup Archive
+    echo "Output\n"
+    echo $?
 
-    rm -f $BACKUP_ARCHIVE
-else
-    echo "Backup archive could not be created..."
-fi
+    # Get The Size Of This File And Store It
 
-exit $BACKUP_STATUS
+    BACKUP_ARCHIVE_SIZE=$(aws s3 ls $BACKUP_ARCHIVE_PATH \
+        --profile=$BACKUP_AWS_PROFILE_NAME \
+        ${BACKUP_AWS_ENDPOINT:+ --endpoint=$BACKUP_AWS_ENDPOINT} | \
+        awk '{print $3}')
+
+    BACKUP_ARCHIVES+=($BACKUP_ARCHIVE_NAME $BACKUP_ARCHIVE_SIZE)
+done
+
+BACKUP_ARCHIVES_JSON=$(echo "[$(printf '{\"%s\": %d},' ${BACKUP_ARCHIVES[@]} | sed '$s/,$//')]")
+
+curl -s --request POST \
+    --url "$FORGE_PING_CALLBACK" \
+    --data-urlencode "type=backup" \
+    --data-urlencode "backup_token=$BACKUP_TOKEN" \
+    --data-urlencode "streamed=true" \
+    --data-urlencode "status=" \
+    --data-urlencode "backup_configuration_id=$BACKUP_ID" \
+    --data-urlencode "archives=$BACKUP_ARCHIVES_JSON" \
+    --data-urlencode "archive_path=$BACKUP_FULL_STORAGE_PATH$BACKUP_TIMESTAMP" \
+    --data-urlencode "started_at=$SCRIPT_STARTED_AT" \
+    --data-urlencode "uuid=$BACKUP_UUID"
+
+exit 0
